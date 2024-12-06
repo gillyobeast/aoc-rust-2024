c9050c99d34550c0c934e7c0d71cd209e58bac02
Lake Armitage <lake.armitage@anaplan.com>
2024-12-06 08:57:05 +0000

    day 4 setup

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -1,8 +1,15 @@
 advent_of_code::solution!(4);
 use anyhow::Result;
 
-pub fn part_one(_input: &str) -> Result<usize> {
+pub fn part_one(input: &str) -> Result<usize> {
+    let _matrix = parse(input);
+
     unimplemented!()
+}
+
+fn parse(input: &str) -> Vec<Vec<char>> {
+    let matrix = input.lines().map(|line| line.chars().collect()).collect();
+    dbg!(matrix)
 }
 
 pub fn part_two(_input: &str) -> Result<usize> {
@@ -16,7 +23,7 @@ mod tests {
     #[test]
     fn test_part_one() -> Result<()> {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY))?;
-        assert_eq!(result, 0);
+        assert_eq!(result, 18);
 
         Ok(())
     }
